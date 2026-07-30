256558439f2c0027fc039ab7b819cb82f14b36db
Mamathacoder <mamathwork123@gmail.com>
2026-07-30 11:25:48 +0530

    loops

--- JAVA_10KCoders_Project/src/com/coders/loopsprogram/Loopprogram2.java
+++ JAVA_10KCoders_Project/src/com/coders/loopsprogram/Loopprogram2.java
@@ -4,7 +4,7 @@ public class Loopprogram2 {
 	public static void main(String[] args) {
 		int i=0;
 		do {
-			System.out.println(i + " ");
+			System.out.println(i);
 			
 			//System.out.println("Hey");
 			i++;
