eb86934d2f89a8e662ee26c52c09a1ae18d5fe0b
Mamathacoder <mamathwork123@gmail.com>
2026-08-06 13:03:34 +0530

    Scanner class

---
+++ JAVA_10KCoders_Project/src/com/coders/scannerclass/ScannerClass.java
@@ -0,0 +1,22 @@
+package com.coders.scannerclass;
+
+import java.util.Scanner;//importing it for Scanner class 
+
+public class ScannerClass {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println(sc.next());
+		Scanner input=new Scanner(System.in);
+		System.out.println(input.nextInt());
+		Scanner in=new Scanner(System.in);
+		System.out.println(in.nextInt());
+		
+		int a=sc.nextInt();
+		
+		float b=sc.nextFloat();
+		
+		
+		
+	}
+
+}
