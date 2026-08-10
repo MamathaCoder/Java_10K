cfd54380582d3dd2fc373345f67fbe8487753bb3
Mamathacoder <mamathwork123@gmail.com>
2026-08-10 13:03:07 +0530

    Access Specifiers

---
+++ JAVA_10KCoders_Project/src/com/coders/accessspecifiers/Publicdemo.java
@@ -0,0 +1,25 @@
+package com.coders.accessspecifiers;
+
+public class Publicdemo {
+	public int a=9;  // data member 
+    private String b="Maduri";
+    
+    char w='i';
+    
+    protected int s=7;
+    
+    public void display() {// function or method or member function
+            System.out.println("welcome");
+    }
+    
+    public static void main(String[] args) {
+            Publicdemo p=new Publicdemo();
+            
+            System.out.println(p.a);
+            System.out.println(p.b);
+            System.out.println(p.s);
+            System.out.println(p.w);
+            
+    }
+
+}
