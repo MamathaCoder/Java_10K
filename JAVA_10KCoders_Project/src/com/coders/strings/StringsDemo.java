8fbe857be2bdd8868c37dc536db25105835e4751
Mamathacoder <mamathwork123@gmail.com>
2026-08-12 11:09:41 +0530

    Strings

---
+++ JAVA_10KCoders_Project/src/com/coders/strings/StringsDemo.java
@@ -0,0 +1,27 @@
+package com.coders.strings;
+
+public class StringsDemo {
+	public static void main(String[] args) {
+		
+		String s="Coders";//assign directly 
+		
+		String stn=new String();//user input string 
+		
+		String stm=new String("Coder");//
+		
+		System.out.println(s);
+		
+		System.out.println(stm);
+		
+		System.out.println(s.charAt(0));
+		
+		System.out.println(s.toUpperCase());
+		
+		System.out.println(s.stripLeading());//gives only string
+		
+		System.out.println(s.length());
+		
+		
+	}
+
+}
